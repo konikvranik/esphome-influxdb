e9fd04abac7444b47d0983a4af310884abf81edb
Petr Vraník <Petr.Vranik@mgm-tp.com>
2025-06-01 21:29:05 +0200

    light

--- components/influxdb2/__init__.py
+++ components/influxdb2/__init__.py
@@ -2,7 +2,7 @@ import re
 
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID, CONF_PORT
+from esphome.const import CONF_ID, CONF_PORT, CONF_BINARY_SENSORS, CONF_SWITCHES, CONF_LIGHT, CONF_TEXT_SENSORS
 from esphome.core import CORE
 from esphome.core import coroutine_with_priority
 
@@ -79,7 +79,8 @@ def to_code(config):
     cg.add(var.set_tags(join_tags(config[CONF_TAGS])))
     cg.add(var.set_precision(config[CONF_PRECISION]))
 
-    for sensor_id, sensor_config in config[CONF_SENSORS].items():
+    for sensor_id, sensor_config in [*config[CONF_SENSORS], *config[CONF_BINARY_SENSORS], *config[CONF_SWITCHES],
+                                     *config[CONF_LIGHT].items(), *config[CONF_TEXT_SENSORS].items()]:
         if not sensor_config[CONF_IGNORE]:
             tags = join_tags({**config[CONF_TAGS], **sensor_config[CONF_TAGS]})
             field_key = sensor_config[CONF_FIELD_KEY]
@@ -100,4 +101,5 @@ def to_code(config):
 
 def join_tags(config):
     return ''.join(
-        ',{}={}'.format(re.sub(r'\s', r'\\\\\g<0>', tag), re.sub(r'\s', r'\\\\\g<0>', value)) for tag, value in config.items())
+        ',{}={}'.format(re.sub(r'\s', r'\\\\\g<0>', tag), re.sub(r'\s', r'\\\\\g<0>', value)) for tag, value in
+        config.items())
